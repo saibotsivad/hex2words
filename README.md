f42592f7f188a361b56c2eef2a9eafd40b1eca9e
Tobias Davis <saibotsivad@Tobiass-MacBook-Air.local>
2014-03-06 21:44:01 -0600

    Init with tests!

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-hex2words
-=========
+Given a string of hex values, a PGP thumbprint or SHA
+hash for example, return an array of PGP words.
 
-Given a string of hex values, a PGP thumbprint or SHA hash for example, return an array of PGP words.
+	var words = parse('E582')
+
+Will return
+
+	[
+		'topmost',
+		'Istanbul'
+	]
---
+++ four_letters.js
@@ -0,0 +1,17 @@
+var word_list = require('pgp-word-list')
+
+module.exports = function fourLetters(four_letters) {
+	var first_so_called_byte = parseInt(four_letters.substr(0, 2), 16)
+	if (isNaN(first_so_called_byte)) {
+		throw new Error('first byte invalid')
+	}
+
+	var second_so_called_byte = parseInt(four_letters.substr(2, 2), 16)
+	if (isNaN(second_so_called_byte)) {
+		throw new Error('second byte invalid')
+	}
+
+	var first_word = word_list[first_so_called_byte][0]
+	var second_word = word_list[second_so_called_byte][1]
+	return [first_word, second_word]
+}
